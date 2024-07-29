b2cd3c2cb25132e369537056b0288c61329fe23e
shirakou <koudai126126@icloud.com>
2024-07-29 22:41:03 +0900

    Herokuへのデプロイ準備

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (APP::enviroment(['production'])) {
+        if (APP::environment(['production'])) {
             URL::forceScheme('https');
         }
     }
