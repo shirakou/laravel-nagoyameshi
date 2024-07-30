5c1455b78e7718f28d8b966831723ed62b18afe2
shirakou <koudai126126@icloud.com>
2024-07-30 18:33:48 +0900

    Herokuへのデプロイ準備

---
+++ database/migrations/2024_07_30_183131_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $table->string('kana')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable() ;
+            $table->date('birthday')->nullable();
+            $table->string('occupation')->nullable();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->dropColumn('kana');
+        $table->dropColumn('postal_code');
+        $table->dropColumn('address');
+        $table->dropColumn('phone_number');
+        $table->dropColumn('birthday');
+        $table->dropColumn('occupation');
+    }
+};
